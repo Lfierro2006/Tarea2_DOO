adf464a818ae2b7f1adb3686e2f6cc45e41eea42
Lfierro2006 <lfierro2025@inf.udec.cl>
2026-05-19 19:05:06 -0400

    crear versión inicial Departamento

---
+++ src/main/java/departamento/Departamento.java
@@ -0,0 +1,26 @@
+package departamento;
+import empleado.Empleado;
+
+import java.util.ArrayList;
+
+public class Departamento {
+    private final String nombre;
+    private ArrayList<Empleado> trabajadores;
+
+    public Departamento(String nombre){
+        this.nombre=nombre;
+    }
+
+    public int obtenerCantidadEmpleados(){
+        return trabajadores.size();
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void addEmpleado(Empleado empleado){
+        trabajadores.add(empleado);
+    }
+
+}
