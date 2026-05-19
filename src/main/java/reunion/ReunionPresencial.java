7872b3482bf22c535e8cb61919f2ae5e15c68c74
Lfierro2006 <lfierro2025@inf.udec.cl>
2026-05-19 19:06:09 -0400

    Crear subclase ReunionPresencial

---
+++ src/main/java/reunion/ReunionPresencial.java
@@ -0,0 +1,11 @@
+package reunion;
+
+import empleado.Empleado;
+
+public class ReunionPresencial extends Reunion {
+    private String sala;
+    public ReunionPresencial(String sala, Empleado organizador){
+        this.sala=sala;
+        super(organizador);
+    }
+}
