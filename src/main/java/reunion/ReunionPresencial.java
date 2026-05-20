fd23af4b1b76751a8e69ec61ec27960c25971d42
Lfierro2006 <lfierro2025@inf.udec.cl>
2026-05-19 23:57:21 -0400

    corregir nombres variables

--- src/main/java/reunion/ReunionPresencial.java
+++ src/main/java/reunion/ReunionPresencial.java
@@ -7,13 +7,13 @@ import java.time.Instant;
 import java.util.Date;
 
 public class ReunionPresencial extends Reunion {
-    private String ubicacion;
+    private String sala;
     public ReunionPresencial(Empleado organizador,String sala, Date fecha, Instant horaPrevista, Duration duracionPrevista, tipoReunion tipoReunion/*, List invitacion*/){
         super(organizador,fecha,horaPrevista,duracionPrevista,tipoReunion/*, invitaciones*/);
-        this.ubicacion=sala;
+        this.sala=sala;
     }
     public String getSala() {
-        return ubicacion;
+        return sala;
     }
 
 }
--- src/main/java/reunion/ReunionVirtual.java
+++ src/main/java/reunion/ReunionVirtual.java
@@ -7,12 +7,12 @@ import java.time.Instant;
 import java.util.Date;
 
 public class ReunionVirtual extends Reunion {
-    private String ubicacion;
+    private String enlace;
     public ReunionVirtual(Empleado organizador,String link, Date fecha, Instant horaPrevista, Duration duracionPrevista, tipoReunion tipoReunion/*, List invitacion*/){
         super(organizador,fecha,horaPrevista,duracionPrevista,tipoReunion/*, invitaciones*/);
-        this.ubicacion=link;
+        this.enlace=link;
     }
     public String getLink(){
-        return ubicacion;
+        return enlace;
     }
 }
