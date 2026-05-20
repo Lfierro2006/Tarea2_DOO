85cde4928fc6a77377da098f624e824db14180f6
Lfierro2006 <lfierro2025@inf.udec.cl>
2026-05-19 22:32:48 -0400

    Probar libreria Time

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,15 @@
-import empleado.Empleado;
-import reunion.Reunion;
+//import empleado.Empleado;
+//import reunion.Reunion;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
+import static java.time.LocalTime.*;
 
 public class Main {
     public static void main(String[] args){
+        DateTimeFormatter formatter= DateTimeFormatter.ofPattern("HH:mm:ss");
+        LocalTime Hora=LocalTime.now();
+        String hora= Hora.format(formatter);
+        System.out.println(hora);
     }
 }
