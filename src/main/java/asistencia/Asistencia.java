ed0ce27e5baa08b586eba3f09af7a2022c5ff159
Lfierro2006 <lfierro2025@inf.udec.cl>
2026-05-21 04:18:02 -0400

    Hacer implementación de Asistencia

--- src/main/java/asistencia/Asistencia.java
+++ src/main/java/asistencia/Asistencia.java
@@ -1,5 +1,17 @@
 package asistencia;
 
 public class Asistencia {
+    private final Invitable invitado;
+    private final Instant horaLlegada;
+    public Asistencia(Invitable invitado,Instant horaLlegada){
+        this.invitado=invitado;
+        this.horaLlegada=horaLlegada;
+    }
 
+    public Instant getHoraLlegada(){
+        return horaLlegada;
+    }
+    public Invitable getInvitado(){
+        return invitado;
+    }
 }
