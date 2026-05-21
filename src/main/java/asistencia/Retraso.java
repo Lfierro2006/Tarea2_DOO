9df4715705abdf4d53283e23b8579b29b55a8b0f
Lfierro2006 <lfierro2025@inf.udec.cl>
2026-05-21 04:25:57 -0400

    implementar la sub clase retraso

--- src/main/java/asistencia/Retraso.java
+++ src/main/java/asistencia/Retraso.java
@@ -1,4 +1,12 @@
 package asistencia;
 
 public class Retraso {
+import invitable.Invitable;
+
+import java.time.Instant;
+
+public class Retraso extends Asistencia {
+    public Retraso(Invitable invitado, Instant horaLlegada){
+        super(invitado,horaLlegada);
+    }
 }
