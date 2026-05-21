a8e0b98adbb5a75e94724acba61d87aa8f8e5791
Lfierro2006 <lfierro2025@inf.udec.cl>
2026-05-21 04:41:56 -0400

    Actualizar constructores de las subclases de Reunion

--- src/main/java/reunion/ReunionPresencial.java
+++ src/main/java/reunion/ReunionPresencial.java
@@ -1,15 +1,16 @@
 package reunion;
 
 import empleado.Empleado;
-
+import invitacion.Invitacion;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Date;
+import java.util.List;
 
 public class ReunionPresencial extends Reunion {
     private String sala;
-    public ReunionPresencial(Empleado organizador,String sala, Date fecha, Instant horaPrevista, Duration duracionPrevista, tipoReunion tipoReunion/*, List invitacion*/){
-        super(organizador,fecha,horaPrevista,duracionPrevista,tipoReunion/*, invitaciones*/);
+    public ReunionPresencial(Empleado organizador, String sala, Date fecha, Instant horaPrevista, Duration duracionPrevista, tipoReunion tipo, List<Invitacion> invitaciones){
+        super(organizador,fecha,horaPrevista,duracionPrevista,tipo,invitaciones);
         this.sala=sala;
     }
     public String getSala() {
--- src/main/java/reunion/ReunionVirtual.java
+++ src/main/java/reunion/ReunionVirtual.java
@@ -1,15 +1,17 @@
 package reunion;
 
 import empleado.Empleado;
+import invitacion.Invitacion;
 
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Date;
+import java.util.List;
 
 public class ReunionVirtual extends Reunion {
     private String enlace;
-    public ReunionVirtual(Empleado organizador,String link, Date fecha, Instant horaPrevista, Duration duracionPrevista, tipoReunion tipoReunion/*, List invitacion*/){
-        super(organizador,fecha,horaPrevista,duracionPrevista,tipoReunion/*, invitaciones*/);
+    public ReunionVirtual(Empleado organizador, String link, Date fecha, Instant horaPrevista, Duration duracionPrevista, tipoReunion tipoReunion, List<Invitacion> invitaciones){
+        super(organizador,fecha,horaPrevista,duracionPrevista,tipoReunion,invitaciones);
         this.enlace=link;
     }
     public String getLink(){
