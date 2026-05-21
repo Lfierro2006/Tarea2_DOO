3c22d3ccd6cac55220107c6f65d85fc585962caf
Lfierro2006 <lfierro2025@inf.udec.cl>
2026-05-21 04:25:14 -0400

    Override toString de PGenerico

--- src/main/java/empleado/PGenerico.java
+++ src/main/java/empleado/PGenerico.java
@@ -4,8 +4,9 @@ public class PGenerico extends Persona{
     public PGenerico(String nombre, String apellidos, String correo){
         super(nombre, apellidos, correo);
     }
+
     @Override
     public String toString(){
-        return "Nombre completo: "+this.getNombre()+" "+this.getApellidos()+ "\n" + "Correo: "+this.getCorreo();
+        return "Nombre completo: "+this.getNombreCompleto()+ "\n" + "Correo: "+this.getCorreo();
     }
 }
